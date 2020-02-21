119ec7eef7c04601cc059985b345e7ab8664de51
rice870 <61168494+rice870@users.noreply.github.com>
2020-02-21 13:18:42 +1030

    new

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,33 @@
 // Arduino Light Show - Patrick and Isaac
 
 // Pin variables so you can easily change
-static int RGB_RED = 12
-static int RGB_BLUE = 11
-static int RGB_GREEN = 10
+static int RGB_RED = 6;
+static int RGB_BLUE = 11;
+static int RGB_GREEN = 10;
+int joe = 0;
+static int RED_1 = 9;
+static int RED_2 = 8;
 
-static int RED_1 = 9
-static int RED_2 = 8
-
-static int SPEAKER = 7
+static int SPEAKER = 7;
 
 
 void setup() {
-  
+  pinMode(RGB_RED, OUTPUT);
+  pinMode(RGB_GREEN, OUTPUT);
+  pinMode(RGB_BLUE, OUTPUT);
+  pinMode(RED_1, OUTPUT);
+  pinMode(RED_2, OUTPUT);
+  pinMode(SPEAKER, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  analogWrite(RGB_RED, joe);
+  joe = joe + 1;
+  if (joe>255) {
+    joe = 0;
+  }
+  Serial.write(joe);
+  analogWrite(RGB_BLUE, 0);
+  analogWrite(RGB_GREEN, 0);
 }
