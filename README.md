aa29a96f71c8f4464c96768429221a47f9e0d620
Orij56 <55019674+Orij56@users.noreply.github.com>
2020-02-21 12:32:53 +1030

    Starting stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+# Arduino School Project
+Arduino light display project
+
+This repo is simply for convenience in a group project don't take it seriously.
---
+++ main/main.ino
@@ -0,0 +1,20 @@
+// Arduino Light Show - Patrick and Isaac
+
+// Pin variables so you can easily change
+static int RGB_RED = 12
+static int RGB_BLUE = 11
+static int RGB_GREEN = 10
+
+static int RED_1 = 9
+static int RED_2 = 8
+
+static int SPEAKER = 7
+
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
