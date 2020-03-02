fa2040ad47524821d4ff4bad269c55b8e0aa0e7a
rice870 <61168494+rice870@users.noreply.github.com>
2020-03-02 17:32:51 +1030

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,5 @@
 // Arduino Light Show - Patrick and Isaac
-#include <time.h>
+#include<time.h>
 
 // Pin variables so you can easily change
 const int RGB_RED = 11;
@@ -9,12 +9,12 @@ const int RGB_BLUE = 9;
 const int RED_1 = 7;
 const int RED_2 = 6;
 const int BUTTON = 5;
-
+int state = 0;
 int buttonstate = 0;
 int counter = 0;
 int r = rand() % 255, g = rand() % 255, b = rand() % 255;
-
 void setup() {
+  srand(time(NULL));
   pinMode(RGB_RED, OUTPUT);
   pinMode(RGB_GREEN, OUTPUT);
   pinMode(RGB_BLUE, OUTPUT);
@@ -22,7 +22,6 @@ void setup() {
   pinMode(RED_2, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
   Serial.begin(9600);
-  srand(time(NULL));
 }
 
 // Static flash fuction to prevent code repetition
@@ -41,18 +40,15 @@ static void rgbDisplay(int r, int g, int b){
 }
 
 void loop() {
-  buttonstate = digitalRead(BUTTON);
   rgbDisplay(r, g, b);
-  if (buttonstate == LOW) {
-      flash(RED_1, 100);
+  if (state == 0 && digitalRead(BUTTON) == LOW) {
+      flash(RED_1, 200);
       delay(100);
-      flash(RED_2, 100);
+      flash(RED_2, 200);
       r = rand() % 255, g = rand() % 255, b = rand() % 255;
-  } else{
-    counter ++;
-    if (counter == 20000 ) {
-      r = rand() % 255, g = rand() % 255, b = rand() % 255;
-      counter = 0;
-    }
+      state = 1;
+  } 
+  if (state == 1 && digitalRead(BUTTON) == HIGH) {
+    state = 0;
   }
 }
