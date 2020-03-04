b4b15c9e3407720499b0f934dee55f41d21e0818
Orij56 <55019674+Orij56@users.noreply.github.com>
2020-03-04 11:14:57 +1030

    good

--- main/main.ino
+++ main/main.ino
@@ -1,27 +1,27 @@
 // Arduino Light Show - Patrick and Isaac
-#include<time.h>
 
 // Pin variables so you can easily change
 const int RGB_RED = 11;
 const int RGB_GREEN = 10;
 const int RGB_BLUE = 9;
-
 const int RED_1 = 7;
 const int RED_2 = 6;
 const int BUTTON = 5;
-int state = 0;
-int buttonstate = 0;
-int counter = 0;
-int r = rand() % 255, g = rand() % 255, b = rand() % 255;
+
+// Storing both button state in previous loop and current loop
+int oldButton = 0;
+int newButton = 0;
+
+int toggle = 0;
+
 void setup() {
-  srand(time(NULL));
+  // Setting up pins
   pinMode(RGB_RED, OUTPUT);
   pinMode(RGB_GREEN, OUTPUT);
   pinMode(RGB_BLUE, OUTPUT);
   pinMode(RED_1, OUTPUT);
   pinMode(RED_2, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
-  Serial.begin(9600);
 }
 
 // Static flash fuction to prevent code repetition
@@ -40,19 +40,21 @@ static void rgbDisplay(int r, int g, int b){
 }
 
 void loop() {
-  if (digitalRead(BUTTON) == LOW) {
-    while (digitalRead(BUTTON) == LOW) {
-      ;
-    }
-    void loop() {
-      rgbDisplay(r, g, b)
+  // buttonNew becomes the buttons state at the beginning of each loop
+  newButton = digitalRead(BUTTON);
+
+  // If button is pressed this loop and button wasn't pressed last loop
+  if (buttonNew == 0 && buttonOld == 1) {
+    // Toggle becomes not toggle: eg. toggling from 1 to 0
+    toggle = !toggle;
+  }
+
+  if (toggle == 1) {
+      // RGB LED displays random colour
+      rgbDisplay(rand() % 255, rand() % 255, rand() % 255);
       flash(RED_1, 200);
-      delay(100);
       flash(RED_2, 200);
-      r = rand() % 255, g = rand() % 255, b = rand() % 255;
-      if (digitalRead(BUTTON) == LOW) {
-        break;
-      }
-    }
-  } 
+  }
+  // buttonOld becomes the buttons state at the end of each loop
+  oldButton = newButton;
 }
